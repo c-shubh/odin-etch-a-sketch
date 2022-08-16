009f209fff2e9641a922c117a9b295f8297adbe9
Shubh Chudasama <65857597+c-shubh@users.noreply.github.com>
2022-08-16 22:59:01 +0530

    Implement a basic hover based etch-a-sketch

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
+let gridSize = 16;
+
+fillGrid();
+
+function changeCellBGColor(e: Event) {
+  (e.target as HTMLDivElement).style.backgroundColor = 'black';
+}
+
+function fillGrid() {
+  const grid = <HTMLDivElement>document.querySelector('.cell-grid')!;
+  const cellSize = grid.clientWidth / gridSize;
+  // the .cell class sets the cell to value of this property
+  grid.style.setProperty('--cellSize', cellSize + 'px');
+  const numCells = gridSize ** 2;
+
+  // create cells
+  for (let i = 0; i < numCells; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    // leave a trail behind when hovering over the cells
+    cell.addEventListener('mouseover', changeCellBGColor)
+    grid.append(cell);
+  }
+}
+
 export {};
